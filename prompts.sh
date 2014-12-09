8bde360d41e8fbe19e50d553bf27e95b291c34d3
Anton Verinov <git@zemlan.in>
2014-12-09 18:05:40 +0200

    execution date in RPROMPT

--- prompts.sh
+++ prompts.sh
@@ -18,3 +18,33 @@ __colorcode+=$(
 )
 
 RPROMPT='%{$__colorcode%}%n%{$reset_color%}'
+
+
+# http://stackoverflow.com/a/26585789
+strlen () {
+    FOO=$1
+    local zero='%([BSUbfksu]|([FB]|){*})'
+    LEN=${#${(S%%)FOO//$~zero/}}
+    echo $LEN
+}
+
+# show right prompt with date ONLY when command is executed
+preexec () {
+    DATE=$( date --rfc-3339=seconds )
+    local len_right=$( strlen "$DATE" )
+    local right_start=$(($COLUMNS - $len_right - 1))
+
+    local len_cmd=$( strlen "$@" )
+    local len_prompt=$(strlen "$PROMPT" )
+    local len_left=$(($len_cmd+$len_prompt))
+
+    RDATE="\033[${right_start}C ${DATE}"
+
+    if [ $len_left -lt $right_start ]; then
+        # command does not overwrite right prompt
+        # ok to move up one line
+        echo -e "\033[1A${__colorcode}${RDATE}${reset_color}"
+    else
+        echo -e "${__colorcode}${RDATE}${reset_color}"
+    fi
+}
